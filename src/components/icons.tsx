f3f435edb2d5a85a69e3bf4078b359509fa3142d
Weply Indonesia <matchboxdevelopment@gmail.com>
2025-06-24 00:23:08 +0000

    gunakan saja logo seperti nama script sebelumnya dimana ada beberapa dot

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,4 +1,4 @@
-export const ScriptIcon = (props: React.SVGProps<SVGSVGElement>) => (
+export const BitoIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg 
       width="24" 
       height="24" 
@@ -15,28 +15,5 @@ export const ScriptIcon = (props: React.SVGProps<SVGSVGElement>) => (
       <circle cx="16" cy="16" r="2" fill="currentColor"/>
       <circle cx="8" cy="16" r="2" fill="currentColor"/>
       <circle cx="12" cy="16" r="2" fill="currentColor"/>
-    </svg>
-);
-
-
-export const BitoIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        xmlns="http://www.w3.org/2000/svg"
-        {...props}
-    >
-        <path d="M16.6 8.4C18.2 9.9 19.1 12 19.2 14.2" />
-        <path d="M16.6 8.4C15.1 6.8 13 5.9 10.8 5.8" />
-        <path d="M16.6 8.4L7.4 17.6" />
-        <path d="M7.4 17.6C5.8 16.1 4.9 14 4.8 11.8" />
-        <path d="M7.4 17.6C8.9 19.2 11 20.1 13.2 20.2" />
-        <path d="M12 12l4.6-3.6" />
     </svg>
 );
